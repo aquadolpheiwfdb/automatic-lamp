53817665b7d81c73421f830edbf29cb15b24de1c
aquadolpheiwfdb <80494293+aquadolpheiwfdb@users.noreply.github.com>
2021-06-21 09:12:22 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBTm0ZoGbsK5YidDB8Z50oRR3U332cOQEE",
+    authDomain: "kwitter-webapp-part-2.firebaseapp.com",
+    databaseURL: "https://kwitter-webapp-part-2-default-rtdb.firebaseio.com",
+    projectId: "kwitter-webapp-part-2",
+    storageBucket: "kwitter-webapp-part-2.appspot.com",
+    messagingSenderId: "289199773415",
+    appId: "1:289199773415:web:201b9f2d3b64d5c8633442"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
